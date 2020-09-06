2bf498f664e59af98296b44521f3e4bf91026dd8
Luigi Ciccone <luigiciccone@hotmail.com>
2020-09-05 21:32:33 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,76 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-	return <Title>Explore Repositórios no GitHub</Title>;
+	return (
+		<>
+			<img src={logoImg} alt="Github Explorer" />
+			<Title>Explore Repositórios no GitHub</Title>
+
+			<Form action="">
+				<input placeholder="Digite o nome do repositório" />
+				<button type="submit">Pesquisar</button>
+			</Form>
+
+			<Repositories>
+				<a href="teste">
+					<img
+						src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg"
+						alt="Luigi"
+					/>
+					<div>
+						<strong>
+							rocketseat-education / gostack-template-typeorm-upload
+						</strong>
+						<p>
+							Template para iniciar o desafio de Node.js do nível de "Iniciando
+							back-end do app"
+						</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</a>
+				<a href="teste">
+					<img
+						src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg"
+						alt="Luigi"
+					/>
+					<div>
+						<strong>
+							rocketseat-education / gostack-template-typeorm-upload
+						</strong>
+						<p>
+							Template para iniciar o desafio de Node.js do nível de "Iniciando
+							back-end do app"
+						</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</a>
+				<a href="teste">
+					<img
+						src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg"
+						alt="Luigi"
+					/>
+					<div>
+						<strong>
+							rocketseat-education / gostack-template-typeorm-upload
+						</strong>
+						<p>
+							Template para iniciar o desafio de Node.js do nível de "Iniciando
+							back-end do app"
+						</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</a>
+			</Repositories>
+		</>
+	);
 };
 
 export default Dashboard;
