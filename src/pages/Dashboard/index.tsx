3bc17d47deaa74fb51be3f0d6370903c147a8d28
Luigi Ciccone <luigiciccone@hotmail.com>
2020-09-05 23:09:11 -0300

    Conectando a API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,73 +1,68 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+	full_name: string;
+	description: string;
+	owner: {
+		login: string;
+		avatar_url: string;
+	};
+}
+
 const Dashboard: React.FC = () => {
+	const [newRepo, setNewRepo] = useState('');
+	const [repositories, setRepositories] = useState<Repository[]>([]);
+
+	async function handleAddRepository(
+		event: FormEvent<HTMLFormElement>,
+	): Promise<void> {
+		event.preventDefault();
+
+		const response = await api.get<Repository>(`/repos/${newRepo}`);
+
+		const repository = response.data;
+
+		setRepositories([...repositories, repository]);
+
+		setNewRepo('');
+	}
+
 	return (
 		<>
 			<img src={logoImg} alt="Github Explorer" />
 			<Title>Explore Repositórios no GitHub</Title>
 
-			<Form action="">
-				<input placeholder="Digite o nome do repositório" />
+			<Form onSubmit={handleAddRepository}>
+				<input
+					placeholder="Digite o nome do repositório"
+					value={newRepo}
+					onChange={e => setNewRepo(e.target.value)}
+				/>
 				<button type="submit">Pesquisar</button>
 			</Form>
 
 			<Repositories>
-				<a href="teste">
-					<img
-						src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg"
-						alt="Luigi"
-					/>
-					<div>
-						<strong>
-							rocketseat-education / gostack-template-typeorm-upload
-						</strong>
-						<p>
-							Template para iniciar o desafio de Node.js do nível de "Iniciando
-							back-end do app"
-						</p>
-					</div>
+				{repositories.map(repository => (
+					<a key={repository.full_name} href="teste">
+						<img
+							src={repository.owner.avatar_url}
+							alt={repository.owner.login}
+						/>
+						<div>
+							<strong>{repository.full_name}</strong>
+							<p>{repository.description}</p>
+						</div>
 
-					<FiChevronRight size={20} />
-				</a>
-				<a href="teste">
-					<img
-						src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg"
-						alt="Luigi"
-					/>
-					<div>
-						<strong>
-							rocketseat-education / gostack-template-typeorm-upload
-						</strong>
-						<p>
-							Template para iniciar o desafio de Node.js do nível de "Iniciando
-							back-end do app"
-						</p>
-					</div>
-
-					<FiChevronRight size={20} />
-				</a>
-				<a href="teste">
-					<img
-						src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg"
-						alt="Luigi"
-					/>
-					<div>
-						<strong>
-							rocketseat-education / gostack-template-typeorm-upload
-						</strong>
-						<p>
-							Template para iniciar o desafio de Node.js do nível de "Iniciando
-							back-end do app"
-						</p>
-					</div>
-
-					<FiChevronRight size={20} />
-				</a>
+						<FiChevronRight size={20} />
+					</a>
+				))}
 			</Repositories>
 		</>
 	);
